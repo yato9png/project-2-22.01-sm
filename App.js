0283820b1396af61ec5a5e11f80a8544443d605e
yato9png <164188994+yato9png@users.noreply.github.com>
2025-03-28 17:05:19 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,7 @@
+let a = parseFloat(prompt("Введите коэффициент a:"));
+let b = parseFloat(prompt("Введите коэффициент b:"));
+let c = parseFloat(prompt("Введите коэффициент c:"));
+
+let discr = b * b - 4 * a * c;
+
+alert("Дискриминант D = " + discr.toString());
